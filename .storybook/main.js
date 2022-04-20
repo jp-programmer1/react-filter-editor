97f81c6f5ddb5b1d26bb9059cad23fc2a76ed8c0
Juanpi-Fernandez <jpprogrammer1@gmail.com>
2022-04-19 22:31:30 -0300

    add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,40 @@
+module.exports = {
+  stories: [
+    "../**/*.stories.mdx",
+    "../**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        configureJSX: true,
+      },
+    },
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+  ],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "ts-loader",
+        options: {
+          compiler: 'typescript',
+        }
+      }
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config
+  },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  }
+};
